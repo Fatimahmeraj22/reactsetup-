36e6bfd6e9780864f7ef877e719a4310b75a988c
Mac <mac@Meraj.local>
2025-01-30 16:09:48 +0530

    ~/reactsetup-/client

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import Sub from "./Components/Sub";
+function App() {
+  return (
+    <div>
+      <Sub />
+      <h5>MERAJ FAATIMA?HH</h5>     
+      <div className="card-container">
+        <div className="card">Card</div>
+      </div>
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ client/src/Components/Sub.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Sub() {
+  return <p> HII EVERYONE</p>;
+}
+
+export default Sub;
